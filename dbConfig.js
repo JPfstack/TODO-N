98959eb8ce1af357fcfc3cc9127d4ce8bcc0694f
JPfstack <juanpedro.jimenez@gmail.com>
2020-12-19 13:57:24 +0100

    dbConfig

--- dbConfig.js
+++ dbConfig.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 const connect = () => {
     const pool = mysql.createPool({
-        host: "127.0.0.1",
-        user: "root",
-        password: "",
+        host: "j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+        user: "iqgy0z7oowxx15ip",
+        password: "pvd0mpdhqysrrar5",
         port: 3306,
-        database: "todo"
+        database: "s9q90jl9ash7sm2k"
     });
 
     global.db = pool;
